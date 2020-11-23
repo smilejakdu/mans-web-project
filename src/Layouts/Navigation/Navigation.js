f96f75140bc947ecdb75157c9917c570bdab643b
smilejakdu <ash982416@gmail.com>
2020-11-24 01:08:43 +0900

    badge

--- src/Layouts/Navigation/Navigation.js
+++ src/Layouts/Navigation/Navigation.js
@@ -3,24 +3,35 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { HeaderBorder, Logo } from "./Navigation.styled";
-import { Button, Navbar, Form, FormControl, Nav, Badge } from "react-bootstrap";
+import {
+  Button,
+  Navbar,
+  Form,
+  FormControl,
+  Nav,
+  Badge,
+  NavDropdown,
+} from "react-bootstrap";
 
 const Navigation = () => {
   return (
     <>
-      <Navbar bg="light" variant="light">
-        <Navbar.Brand href="/home">
-          <Badge variant="secondary">Man's Style</Badge>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          <Badge variant="secondary">Man's Beauty</Badge>
         </Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/sale">sale</Nav.Link>
-          <Nav.Link href="/popularity">popularity</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-primary">Search</Button>
-        </Form>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/sale">sale</Nav.Link>
+            <Nav.Link href="/popularity">popularity</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
